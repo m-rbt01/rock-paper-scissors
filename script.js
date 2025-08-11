0b78b73d6cfa39422b974bafcaaf0165e78cbb7f
Christian Donjuan <cristiandjuan@gmail.com>
2025-08-11 16:57:57 -0500

    fix: Return recursive playRound() draw result
    Previously, the playRound() function's response to
    a draw functioned correctly when the user
    enters a new choice, but not in the
    case that the user quits the game after a draw.
    In the case that the user cancels the
    prompt to enter a new choice after a draw, the
    calling playGame() function failed to interpret
    the cancelled choice. This resulted in the playGame()
    function's loop to continue execution into a new
    round. Include a return statement for the recursive
    playRound() function call. This resolves the issue
    and by returning the QUIT sentinel value when the
    user cancels the game after the draw.

--- script.js
+++ script.js
@@ -2,74 +2,80 @@
 const NUM_CHOICES = 3; //number of choices available
 const NUM_ROUNDS = 5; //total number of rounds within a game 
 const ROCK = "rock";
-const SCISSORS = "scissors";
 const PAPER = "paper";
+const SCISSORS = "scissors";
 const QUIT = -1; //user quit sentinel 
 
-//Global Variables
+//GLOBAL VARIABLES
 let userScore = 0;
 let cpuScore = 0;
 
+//***CALCULATE THE COMPUTER'S CHOICE***
 function getComputerChoice(){
     //generate random number between 1 - 3
     let temp = Math.floor(Math.random() * NUM_CHOICES) + 1;
     switch(temp){ //convert random number into corresponding choice
         case 1:
             return ROCK;
-            break;
         case 2:
-            return SCISSORS;
-            break;
-        default:
             return PAPER;
+        default:
+            return SCISSORS;
     }
 }
 
+//***GET THE USER'S CHOICE***
 function getHumanChoice(){
     let temp; //holds user choice
-    try{ //Get user choice, convert to lowercase
+    try{ //Get user choice, remove potential whitespace from ends and convert to lowercase (case-insensitive)
         temp = prompt(`"Rock" | "Paper" | "Scissors"? Please Enter Your Choice: (or click Cancel to quit)`).trim().toLowerCase();
     }
-    catch(error){ //User canceled input
-        alert("Thank you for playing!");
+    catch(error){ //User canceled input, return to the current round
         return QUIT;
     }
-    if((temp != ROCK) && (temp != PAPER) && (temp != SCISSORS)){ //input validation
+    if((temp != ROCK) && (temp != PAPER) && (temp != SCISSORS)){ //validate given input
         alert(`${temp} is not a valid choice.`);
         return getHumanChoice(); //get input again
     }
     return temp; //return valid input
 }
 
+//***DETERMINE THE WINNER OF A SINGLE ROUND***
 function playRound(userChoice, cpuChoice){
-    if(userChoice === QUIT){
-
+    if(userChoice === QUIT){ //cancel the current round if the user quit
+        return QUIT;
     }
-    else{
-        console.log(`User choice: ${userChoice} CPU choice: ${cpuChoice}`); //display player choices
-        if(userChoice === cpuChoice){ //Round draw
-            alert("Round Draw.");
-            playRound(getHumanChoice(), getComputerChoice());
-        }
-        else if((userChoice === ROCK && cpuChoice === SCISSORS) ||
-        (userChoice === SCISSORS && cpuChoice === PAPER) || 
-        (userChoice === PAPER && cpuChoice === ROCK)){ //User won the current round
-            ++userScore;
-            console.log(`You won! ${userChoice} beats ${cpuChoice}`);
-        }
-        else{ //User lost the current round
-            ++cpuScore;
-            console.log(`You lose. ${cpuChoice} beats ${userChoice}`);
-        }
+    //Evaluate the winner of the current round
+    console.log(`User choice: ${userChoice} CPU choice: ${cpuChoice}`); //display player choices
+    if(userChoice === cpuChoice){ //Round draw
+        alert("Round Draw.");
+        return playRound(getHumanChoice(), getComputerChoice()); //restart current round
+    }
+    else if((userChoice === ROCK && cpuChoice === SCISSORS) ||
+    (userChoice === SCISSORS && cpuChoice === PAPER) || 
+    (userChoice === PAPER && cpuChoice === ROCK)){ //User won the current round
+        ++userScore;
+        console.log(`You won! ${userChoice} beats ${cpuChoice}`);
+    }
+    else{ //User lost the current round
+        ++cpuScore;
+        console.log(`You lose. ${cpuChoice} beats ${userChoice}`);
     }
 }
 
+//***INITIATE A GAME***
 function playGame(){
+    //Iterate through the total number of rounds
     for(let i = 1; i <= NUM_ROUNDS; i++){
-        console.log(`-------ROUND ${i}------`);
-        playRound(getHumanChoice(), getComputerChoice()); //play a single round
+        console.log(`-------ROUND ${i}------`); //display the current round
+        if(playRound(getHumanChoice(), getComputerChoice()) === QUIT){ //play a single round, unless the user quit
+            alert("Thank you for playing!");
+            console.log("User quit the game.");
+            break;
+        } 
         console.log(`User Score: ${userScore} CPU Score: ${cpuScore}`); //display updated score
     }
+    //Display the game's winner
     console.log("-------GAME OVER-------")
     if(userScore > cpuScore){ //User won the game
         console.log("You won the game!");
